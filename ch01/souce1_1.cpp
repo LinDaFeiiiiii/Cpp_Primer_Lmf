fdecd5b83606cedb490b09fa7ad39fe1dc468206
LinDaFeiiiiii <linmengfei521@126.com>
2020-10-19 19:28:25 +0800

    add 01

---
+++ ch01/souce1_1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 80280289 on 2020/10/19.
+//
+#include <iostream>
+int main(){
+    std::cout << " enter 2 numbers " << std::endl;
+    int v1 = 0, v2= 0;
+    std::cin >> v1 >> v2;
+    std::cout << " sum of " << v1 << "and" <<v2 << " is " <<v1+v2<<std::endl;
+    return 0;
+}
+
